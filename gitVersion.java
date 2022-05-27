7557d11aa8eb2ad54cce52d20377e8489f90ecda
saccjain <106382297+saccjain@users.noreply.github.com>
2022-05-27 17:05:44 +0530

    initial commit of gitVersion.java

---
+++ gitVersion.java
@@ -0,0 +1,16 @@
+
+public class gitVersion {
+	
+	
+	public static void main( String args[]) {
+		
+		
+		System.out.println("version 1.O");
+		System.out.println("version 1.O test ddiff");
+		System.out.println("version 2.0 test ddiff");
+		
+		System.out.println("version 2.0 test ddiff");
+		
+		System.out.println("added bug 123");
+	}
+}
